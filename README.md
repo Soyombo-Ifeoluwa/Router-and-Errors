c9db5627e4c7eeced80636653ca1be9ddc0114f5
Soyombo Ifeoluwa <108593370+Soyombo-Ifeoluwa@users.noreply.github.com>
2022-10-23 21:50:33 +0100

    React-router and errors

--- README.md
+++ README.md
@@ -1,7 +1,17 @@
 # Router-and-Errors
 
+This is project is a compilation of React and its library.Basically, it is all about:
 
-The Results: https://d5rcd2.csb.app/?id=H4O2M3E
+- Nested Routes.
+  -404 pages.
+  -Error Boundary.
+  -Loading states.
+  -Assesibility.
+  -Disabled states.
+  -Pagination using JSX.
 
+The React Library used in this Project were React-Router-dom and React-Error-Boundary.
+
+The Results: https://5yqzxv.csb.app/?id=H4O2M3E
 
 Created with CodeSandbox
--- src/Pagination/Pagination.js
+++ src/Pagination/Pagination.js
@@ -1,4 +1,5 @@
-import { useState } from 'react';
+import { useState } from "react";
+import { NavLink } from "react-router-dom";
 import Fetch from "./useFetch.js";
 
 function Users() {
@@ -11,11 +12,9 @@ function Users() {
 
   const PER_PAGE = 5;
   const total = data?.results?.length;
-  const pages = 10;
-  
+  const pages = 20;
   const skip = page * PER_PAGE - PER_PAGE;
   console.log(skip, total);
-
 
   if (loading) {
     return <>Loading...</>;
@@ -26,44 +25,49 @@ function Users() {
   }
 
   return (
-    <div className="page">
-      <h1 className="title">List of Users</h1>
-      {data?.results
-      
-        .map((each, index) => {
+    <section>
+      <div className="page">
+        <h1 className="title">List of Users</h1>
+
+        {data?.results.map((each, index) => {
           const name = `${each.name.title} ${each.name.first} ${each.name.last}`;
           return (
-            <li key={name.toLowerCase().replaceAll(' ', '')}>{`${
+            <li key={name.toLowerCase().replaceAll(" ", "")}>{`${
               index + 1
             }.${name}`}</li>
           );
         })}
-      {
-        <button
-          disabled={page <= 1}
-          onClick={() => setPage((prev) => prev - 1)}
-        >
-          prev
-        </button>
-      }
-      <p className="pagination">
-        Pages: {page} of {pages}
-      </p>
-      {
-        <button
-          disabled={page >= pages}
-          aria-disabled={page >= pages}
-          onClick={() => setPage((prev) => prev + 1)}
-        >
-          next
-        </button>
-      }
-      {Array.from({ length: pages }, (value, index) => index + 1).map(
-        (each) => (
+        <p className="pagination">
+          Pages: {page} of {pages}
+        </p>
+        {
+          <button
+            disabled={page >= pages}
+            aria-disabled={page >= pages}
+            onClick={() => setPage((prev) => prev + 1)}
+          >
+            next
+          </button>
+        }
+        {/* another magic here */}
+        {Array.from({ length: pages }, (_, index) => index + 1).map((each) => (
           <button onClick={() => setPage(each)}>{each}</button>
-        )
-      )}
-    </div>
+        ))}
+        {
+          <button
+            disabled={page <= 1}
+            onClick={() => setPage((prev) => prev - 1)}
+          >
+            prev
+          </button>
+        }
+      </div>
+      <div className="Home-btn">
+        <NavLink to="/" className="navlink1">
+          Home
+        </NavLink>
+      </div>
+    </section>
   );
 }
 
